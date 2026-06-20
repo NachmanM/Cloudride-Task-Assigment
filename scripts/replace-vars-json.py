c54a5669ee956cc84d5e7d6b0f344f9f026caf89
NachmanM <nachman.molko@gmail.com>
2026-06-20 18:11:37 +0300

    changed tf version in gh action to same as local pc

--- scripts/replace-vars-json.py
+++ scripts/replace-vars-json.py
@@ -1,19 +1,21 @@
 import os
 
 src_file_path = "../terraform/task-definitions/nginx-docker-template.json"
-dest_file_path = "../terraform/task-definitions/nginx-docker-new.json"
+dest_file_path = "../terraform/task-definitions/nginx-docker.json"
 
 resource_name = os.getenv("resource_name")
 repo_url = os.getenv("repo_url")
 cloudwatch_log_group = os.getenv("cloudwatch_log_group")
 region = os.getenv("region")
+image_tag = os.getenv("image_tag")
 
 load_vars = {
     r"{{ resource_name }}": resource_name,
     r"{{ name }}": "hello-world-nachman",
     r"{{ repo_url }}": repo_url,
     r"{{ cloudwatch_log_group }}": cloudwatch_log_group,
-    r"{{ region }}": region
+    r"{{ region }}": region,
+    r"{{ image_tag }}": image_tag
 }
 
 with open(src_file_path, "r", encoding="utf-8") as file:
