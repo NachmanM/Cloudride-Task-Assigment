dcff0b7434ee8acc14682a44a2d7de2df5b7914f
NachmanM <nachman.molko@gmail.com>
2026-06-20 16:24:11 +0300

    push to ecr

--- .github/scripts/push-ecr.sh
+++ .github/scripts/push-ecr.sh
@@ -2,10 +2,6 @@
 
 TAG=$1
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 753392824297.dkr.ecr.us-east-1.amazonaws.com
+docker build -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${TAG} nginx-docker
 
-docker build -t prod-default-project-name ../nginx-docker
-
-docker tag prod-default-project-name:${TAG} 753392824297.dkr.ecr.us-east-1.amazonaws.com/prod-default-project-name:latest
-
-docker push 753392824297.dkr.ecr.us-east-1.amazonaws.com/prod-default-project-name:${TAG}
+docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${TAG}
