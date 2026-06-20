4d668fded0cce0b3c17b66ad1b2e858ba3ff3011
NachmanM <nachman.molko@gmail.com>
2026-06-20 16:14:56 +0300

    oidc test

---
+++ .github/scripts/push-ecr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TAG=$1
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 753392824297.dkr.ecr.us-east-1.amazonaws.com
+
+docker build -t prod-default-project-name ../nginx-docker
+
+docker tag prod-default-project-name:${TAG} 753392824297.dkr.ecr.us-east-1.amazonaws.com/prod-default-project-name:latest
+
+docker push 753392824297.dkr.ecr.us-east-1.amazonaws.com/prod-default-project-name:${TAG}
